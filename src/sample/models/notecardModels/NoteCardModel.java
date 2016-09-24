9fefca23c04466f8e33069aceb57a72233ba2f45
rn046359 <ryan.neff@cerner.com>
2016-09-24 14:02:53 -0500

    HOT_MODELS added INSERT/UPDATE/DELETE functions

--- src/sample/models/notecardModels/NoteCardModel.java
+++ src/sample/models/notecardModels/NoteCardModel.java
@@ -1,11 +1,11 @@
 package sample.models.notecardModels;
 
-import com.sun.tools.corba.se.idl.constExpr.Not;
 import sample.models.DbConnectionManager;
 import sample.models.notecardModels.noteCards.NoteCard;
 import sample.models.notecardModels.noteCards.Stack;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,7 +25,7 @@ public class NoteCardModel {
         connection = DbConnectionManager.connect();
     }
 
-    public Stack getSingleStack(String stackName, String userId) {
+    public Stack getSingleStack(final String stackName, final String userId) {
         try {
          final String query = "SELECT id, name, course, subject, date_created, date_modified " +
                               "FROM stacks WHERE name = '"+ stackName +
@@ -35,7 +35,8 @@ public class NoteCardModel {
          final Statement stmt = connection.createStatement();
          final ResultSet resultSet = stmt.executeQuery(query);
          final Stack stack = new Stack();
-         if(resultSet.next()) {
+
+            if(resultSet.next()) {
              stack.setName(resultSet.getString("name"));
              stack.setCourse(resultSet.getString("course"));
              stack.setSubject(resultSet.getString("subject"));
@@ -50,7 +51,7 @@ public class NoteCardModel {
         return null;
     }
 
-    public Map<String, Stack> getStacksByCourse(String course, String userId) {
+    public Map<String, Stack> getStacksByCourse(final String course, final String userId) {
         try {
         final String query = "SELECT id, name, course, subject, date_created, date_modified " +
                              "FROM stacks WHERE course = '"+ course +
@@ -80,7 +81,7 @@ public class NoteCardModel {
         return null;
     }
 
-    public Map<String,Stack> getStacksBySubject(String course, String subject, String userId) {
+    public Map<String,Stack> getStacksBySubject(final String course, final String subject, final String userId) {
         try {
             final String query = "SELECT id, name, course, subject, date_created, date_modified " +
                                 "FROM stacks, WHERE course = '"+ course +
@@ -112,7 +113,7 @@ public class NoteCardModel {
 
     }
 
-    private List<NoteCard> getNoteCardsForStack(String id, String uid ) {
+    private List<NoteCard> getNoteCardsForStack(final String id, final String uid ) {
         try {
            final String query = "SELECT front, back, stack_index FROM notecard " +
                                 "WHERE stack_id = " + id +
@@ -136,6 +137,157 @@ public class NoteCardModel {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public boolean createNoteCard(final NoteCard noteCard, final String userId) {
+        try{
+            final String query = "INSERT INTO notecard (id, front, back, stack_id, stack_index, user_id " +
+                                 "VALUES (?, ?, ?, ?, ?, ?)";
+
+            final PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, noteCard.getId());
+            stmt.setString(2, noteCard.getFront());
+            stmt.setString(3, noteCard.getBack());
+            stmt.setString(4, noteCard.getStackId());
+            stmt.setInt(5, noteCard.getStackIndex());
+            stmt.setString(6, userId);
+
+            final int rowsInserted = stmt.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("A new note card was inserted successfully!");
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean createStack(final Stack stack, final String userId) {
+        try {
+            final String query = "INSERT INTO stacks (id, name, date_modified, date_created, course, subject, user_id " +
+                                 "VALUES (?, ?, ?, ?, ?, ?, ?)";
+            final PreparedStatement stmt = connection.prepareStatement(query);
+
+            stmt.setString(1, stack.getId());
+            stmt.setString(2, stack.getName());
+            stmt.setString(3, stack.getDateModified());
+            stmt.setString(4, stack.getDateCreated());
+            stmt.setString(5, stack.getCourse());
+            stmt.setString(6, stack.getSubject());
+            stmt.setString(7, userId);
+
+            final int rowsInserted = stmt.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("A new note card was inserted successfully!");
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateNoteCard(NoteCard updatedNoteCard, String userId) {
+        try{
+            final String query = "UPDATE notecard " +
+                                 "SET front=?, back=?, stack_index=? "+
+                                 "WHERE stack_id=? " +
+                                 "AND id=?" +
+                                 "AND user_id=?";
+
+            final PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, updatedNoteCard.getFront());
+            stmt.setString(2, updatedNoteCard.getBack());
+            stmt.setInt(3, updatedNoteCard.getStackIndex());
+            stmt.setString(4, updatedNoteCard.getStackId());
+            stmt.setString(5, updatedNoteCard.getId());
+            stmt.setString(6, userId);
+
+            int rowsUpdated = stmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("An existing Notecard was updated successfully!");
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateStack(Stack updatedStack, String userId) {
+        try {
+            final String query = "INSERT UPDATE stacks " +
+                                 "SET name=?, date_modified=?, course=?, subject=? " +
+                                 "WHERE id=? " +
+                                 "AND user_id=? ";
+
+            final PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, updatedStack.getName());
+            stmt.setString(2, updatedStack.getDateModified());
+            stmt.setString(3, updatedStack.getCourse());
+            stmt.setString(4, updatedStack.getSubject();
+            stmt.setString(5, updatedStack.getId());
+            stmt.setString(6, userId);
+
+            int rowsUpdated = stmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("An existing Stack was updated successfully!");
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    return false;
+    }
+
+    public boolean deleteNoteCard(NoteCard noteCard, String userId) {
+        try {
+            final String query = "DELETE FROM notecard " +
+                                 "WHERE id=? " +
+                                 "AND user_id=? ";
+
+            final PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, noteCard.getId());
+            stmt.setString(2, userId);
+
+            int rowsUpdated = stmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("An existing Note Card deleted successfully!");
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deleteStack(Stack stack, String userId) {
+        try {
+            final String query = "DELETE FROM stacks " +
+                    "WHERE id=? " +
+                    "AND user_id=?";
+
+            final PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, stack.getId());
+            stmt.setString(2, userId);
+
+            int rowsUpdated = stmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                System.out.println("An existing Stack deleted successfully!");
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     }
 
 }
